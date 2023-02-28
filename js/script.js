5a4746fececc1208adef5e1afb18b321e2a38f16
Virginia Chico <virgi_13_94@hotmail.com>
2023-02-28 16:22:40 +0100

    fetch tecnico, falta get en backend

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
 const btnLogin = document.getElementById("btnLogin");
-
 const modal = document.getElementById("modal");
-
 const btnClose = document.getElementById("btnClose");
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
 
-btnLogin.addEventListener("click", ()=>{
+btnLogin.addEventListener("click", () => {
     modal.classList.add("modal--show");
 })
 
-btnClose.addEventListener("click", ()=>{
+btnClose.addEventListener("click", (email, password) => {
+
+    //ESTE ES EL FETCH CON LA RUTA PARA EL TECNICO. SACA TODOS LOS TÉCNICOS.
+    fetch(`http://localhost:3000/sanitariabd/tecnico/login`)
+        .then((resp) => {
+            return resp.json()
+        })
+        .then((respJSON) => {
+
+            console.log(respJSON);
+            //   respJSON.map((elemento) => {
+            //     cargarProductos(elemento);
+            //     crearTarjetas(elemento);
+
+        })
+
+
+        .catch(error => console.log(error));
+
     modal.classList.remove("modal--show");
 })
 

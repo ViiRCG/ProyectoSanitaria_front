9dcfe5f073700de167bfaf9172eee211a8eb3207
Virginia Chico <virgi_13_94@hotmail.com>
2023-02-28 12:05:39 +0100

    Ventana de Login. Falta JS con acción al pulsar Acceder

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
+const btnLogin = document.getElementById("btnLogin");
+
+const modal = document.getElementById("modal");
 
 
-
-
-
-
-
-
+btnLogin.addEventListener("click", ()=>{
+    modal.classList.add("modal--show");
+})
 
 
 

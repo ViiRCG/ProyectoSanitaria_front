6e9fb412457167cee3331a64aac29acf3b521634
Virginia Chico <virgi_13_94@hotmail.com>
2023-02-28 12:17:46 +0100

    Formulario login

--- js/script.js
+++ js/script.js
@@ -2,10 +2,14 @@ const btnLogin = document.getElementById("btnLogin");
 
 const modal = document.getElementById("modal");
 
+const btnClose = document.getElementById("btnClose");
 
 btnLogin.addEventListener("click", ()=>{
     modal.classList.add("modal--show");
 })
 
+btnClose.addEventListener("click", ()=>{
+    modal.classList.remove("modal--show");
+})
 
 

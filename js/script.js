66c7633d4532bd6289353b6e236fef1deb2e686d
Virginia Chico <virgi_13_94@hotmail.com>
2023-03-02 20:14:50 +0100

    funciones tecnico login a medias

--- js/script.js
+++ js/script.js
@@ -8,25 +8,41 @@ btnLogin.addEventListener("click", () => {
     modal.classList.add("modal--show");
 })
 
-btnClose.addEventListener("click", (email, password) => {
-
     //ESTE ES EL FETCH CON LA RUTA PARA EL TECNICO. SACA TODOS LOS TÉCNICOS.
+const getAllTecnicos =()=>{
     fetch(`http://localhost:3000/sanitariabd/tecnico/login`)
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((respJSON) => {
+    .then((resp) => {
+        return resp.json()
+    })
+    .then((respJSON) => {
 
-            console.log(respJSON);
-            //   respJSON.map((elemento) => {
-            //     cargarProductos(elemento);
-            //     crearTarjetas(elemento);
+        console.log(respJSON);
+        //   respJSON.map((elemento) => {
+        //     cargarProductos(elemento);
+        //     crearTarjetas(elemento);
 
-        })
+    })
 
 
-        .catch(error => console.log(error));
+    .catch(error => console.log(error));
+}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+btnClose.addEventListener("click", (email, password) => {
+    getAllTecnicos();
     modal.classList.remove("modal--show");
 })
 

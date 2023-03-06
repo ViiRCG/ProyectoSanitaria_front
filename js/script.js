515310cf105142111f8e1764b3bd3641f6c79ff5
Virginia Chico <virgi_13_94@hotmail.com>
2023-03-06 20:39:09 +0100

    algunas funciones

--- js/script.js
+++ js/script.js
@@ -6,27 +6,97 @@ const password = document.getElementById("password").value;
 
 
 
-    //ESTE ES EL FETCH CON LA RUTA PARA EL TECNICO. SACA TODOS LOS TÉCNICOS.
-const getAllTecnicos =()=>{
-    fetch(`http://localhost:3000/sanitaria/tecnico/login`)
-    .then((resp) => {
-        return resp.json()
-    })
-    .then((respJSON) => {
+//ESTE ES EL FETCH CON LA RUTA PARA EL TECNICO. SACA TODOS LOS TÉCNICOS.
+const login = () => {
+    fetch("http://localhost:3000/sanitaria/tecnico/login"), {
+        method: "GET",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            email_tecnico: email,
+            password_tecnico: password,
 
-        console.log(respJSON);
-        //   respJSON.map((elemento) => {
-        
-			// 	if (elemento.email && elemento.password) {
+        })
+    }
+        .then((resp) => {
+            return resp.json()
+        })
+        .then((respJSON) => {
 
-    })
+            console.log(respJSON);
+            //   respJSON.map((elemento) => {
+
+            // 	if (elemento.email && elemento.password) {
+
+        })
 
 
-    .catch(error => console.log(error));
+        .catch(error => console.log(error));
+}
+
+//REGISTRO
+const register = () => {
+    fetch("http://localhost:3000/sanitaria/tecnico/register"), {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+
+            // nombre_tecnico: newTecnico.nombre_tecnico,
+            // apellidos_tecnico: newTecnico.apellidos_tecnico,
+            // curso_tecnico: newTecnico.curso_tecnico,
+            // centro_tecnico: newTecnico.centro_tecnico,
+            // email_tecnico: newTecnico.email_tecnico,
+            // password_tecnico: bcrypt.hashSync(newTecnico.password_tecnico, 10)
+
+        })
+    }
+        .then((resp) => {
+            return resp.json()
+        })
+        .then((respJSON) => {
+
+            console.log(respJSON);
+            //   respJSON.map((elemento) => {
+
+            // 	if (elemento.email && elemento.password) {
+
+        })
+
+
+        .catch(error => console.log(error));
 }
 
 
 
+
+
+// fetch("https://jsonplaceholder.typicode.com/posts", {
+//     method: "POST",
+//     headers: {
+//         "Content-type": "application/json"
+//     },
+//     body: JSON.stringify({
+//         title: "Probando",
+//         body: "Probando",
+//         useid: 1,
+//     }
+//     )
+// })
+//     .then((resp) => {
+//         console.log(resp);
+//         return resp.json()
+//     }
+//     )
+
+//     .then((respJSON) => {
+//         console.log("insercción realizada correctamente");
+//         console.log(respJSON);
+//         console.log("--------------------------------");
+//     })
+//     .catch(error => console.log(error));
 
 
 
@@ -45,7 +115,7 @@ btnLogin.addEventListener("click", () => {
 
 
 btnClose.addEventListener("click", (email, password) => {
-    getAllTecnicos();
+    login();
     modal.classList.remove("modal--show");
 })
 
